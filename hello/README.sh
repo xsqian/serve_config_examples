4ad8014c505a790be956e77a64fa50cb2a43049b
xsqian <xsqian@gmail.com>
2024-03-20 09:14:13 -0700

    added model.h5 to the parent dir, that might be the issue for loading model

--- hello/README.sh
+++ hello/README.sh
@@ -1,2 +1,8 @@
 serve run read_file.app
 
+serve build hello.read_file.app -o ray-service.hello.yaml
+
+serve deploy -f ray-service.hello.yaml
+
+kubectl port-forward svc/rayservice-hello-serve-svc 8000
+
