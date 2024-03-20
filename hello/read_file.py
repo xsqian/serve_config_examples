7fc0c9fa1d361c5f5a147bad3f85f649b1153837
xsqian <xsqian@gmail.com>
2024-03-20 10:39:16 -0700

    added __init__() to read_file.py

--- hello/read_file.py
+++ hello/read_file.py
@@ -3,8 +3,10 @@ from ray import serve
 MODEL_PATH = "file.txt"
 @serve.deployment
 class Model:
-    def __call__(self, model_path: str):
+    def __init__(self, model_path: str):
         self.model_path = model_path
-        with open(model_path) as f:
+
+    def __call__(self):
+        with open(self.model_path) as f:
             return f.read().strip()
 app = Model.bind(MODEL_PATH)
