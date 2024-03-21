c79d7f5f895c0624fb1594ce116ad1b8b80012fa
xsqian <xsqian@gmail.com>
2024-03-20 21:47:39 -0700

    try the tf.keras.models.load_model(model_path) in read_file.py

--- hello/read_file.py
+++ hello/read_file.py
@@ -2,13 +2,16 @@ from ray import serve
 from starlette.requests import Request
 import tensorflow as tf
 
-MODEL_PATH = "file.txt"
+MODEL_PATH = "model.h5"
+print(MODEL_PATH)
 @serve.deployment
 class Model:
     def __init__(self, model_path: str):
         self.model_path = model_path
+        self.model = tf.keras.models.load_model(model_path)
 
     def __call__(self):
-        with open(self.model_path) as f:
+
+        with open("file.txt") as f:
             return f.read().strip()
 app = Model.bind(MODEL_PATH)
